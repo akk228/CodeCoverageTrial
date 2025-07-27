291c44a4ad89383448e22e51911d65dc8e0b10fe
Andrei Kovtun <andreykpq@gmail.com>
2025-07-27 10:43:46 +0200

    adds project under test

---
+++ src/SampleLibrary/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SampleLibrary
+{
+    public class Calculator
+    {
+        public double Divide(double number1, double number2)
+        {
+            if (number2 == 0)
+            {
+                throw new ArgumentOutOfRangeException("why r u dividing by zero, stupid fuck?");
+            }
+            
+            return number1 / number2;
+        }
+    }
+}
