3097c24fc0e69d268326f5af5c94d9e69690b000
Andrei Kovtun <andreykpq@gmail.com>
2025-07-27 14:55:01 +0200

    Adds test of calculator

---
+++ test/SampleLibrary.Test/WhenDivide.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace SampleLibrary.Test
+{
+    public class WhenDivide
+    {
+        [Fact]
+        public void ResultIsCorrect()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Divide(2, 4);
+            Assert.Equal((double)0.5, result);
+        }
+    }
+}
