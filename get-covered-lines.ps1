22af49e7de682b5c625f84f790e65a0b2784d699
Andrei Kovtun <andreykpq@gmail.com>
2025-07-30 23:01:52 +0200

    add displaying of covered lines

--- get-covered-lines.ps1
+++ get-covered-lines.ps1
@@ -9,3 +9,18 @@ foreach ($sp in $xml.SelectNodes('//SequencePoint[@vc>0]')) {
     $endLine = $sp.el
     Write-Output \"File: $file, Lines: $startLine-$endLine\"
 }
+
+$gitDiff = git show --unified=0
+$filesChanged = @{}
+$currFile = ''
+foreach ($line in $gitDiff) {
+    if ($line -match '^\+\+\+ b/(.+)$') {
+        $currFile = $Matches[1]
+        $filesChanged[$currFile] = 0
+    } elseif ($line -match '^\+[^\+]' -and $currFile) {
+        $filesChanged[$currFile]++
+    }
+}
+foreach ($file in $filesChanged.Keys) {
+    Write-Output "File: $file, Lines changed: $($filesChanged[$file])"
+}
