e22b7532b80574cb7a585e986c43205458f35877
Andrei Kovtun <andreykpq@gmail.com>
2025-08-12 16:40:07 +0200

    modifies get changed lines to produce correct result based in diff not on show

--- get-changed-lines.ps1
+++ get-changed-lines.ps1
@@ -1,14 +1,28 @@
-$gitDiff = git show --unified=0
-$filesChanged = @{}
-$currFile = ''
-foreach ($line in $gitDiff) {
-    if ($line -match '^\+\+\+ b/(.+)$') {
-        $currFile = $Matches[1]
-        $filesChanged[$currFile] = 0
-    } elseif ($line -match '^\+[^\+]' -and $currFile) {
-        $filesChanged[$currFile]++
+# $gitDiff = git diff --unified=0
+# $filesChanged = @{}
+# $currFile = ''
+# foreach ($line in $gitDiff) {
+#     if ($line -match '^\+\+\+ b/(.+)$') {
+#         $currFile = $Matches[1]
+#         $filesChanged[$currFile] = 0
+#     } elseif ($line -match '^\+[^\+]' -and $currFile) {
+#         $filesChanged[$currFile]++
+#     }
+# }
+# foreach ($file in $filesChanged.Keys) {
+#     Write-Output "File: $file, Lines changed: $($filesChanged[$file])"
+# }
+
+git diff --unified=0 --no-color |
+    ForEach-Object {
+        if ($_ -match '^\+\+\+ b/(.+)') {
+            $file = $matches[1]
+        }
+        elseif ($_ -match '^@@ -\d+(?:,\d+)? \+(\d+)(?:,(\d+))? @@') {
+            $start = [int]$matches[1]
+            $count = if ($matches[2]) { [int]$matches[2] } else { 1 }
+            for ($i = $start; $i -lt $start + $count; $i++) {
+                "$file`:$i"
+            }
+        }
     }
-}
-foreach ($file in $filesChanged.Keys) {
-    Write-Output "File: $file, Lines changed: $($filesChanged[$file])"
-}
